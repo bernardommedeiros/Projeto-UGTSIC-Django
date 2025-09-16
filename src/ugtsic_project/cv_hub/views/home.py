5bf29988bc4313c9baf48a5df3c5c21517989c79
Bernardo de Moura Medeiros <bernardo181105@gmail.com>
2025-09-16 02:18:23 +0000

    feat: create administrative panel for superuser to view all CVs

--- src/ugtsic_project/cv_hub/views/home.py
+++ src/ugtsic_project/cv_hub/views/home.py
@@ -2,9 +2,18 @@ from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models.cv import CV
-from ..services.home import show_cv
+from ..services.home import show_cv, get_all_cvs
 
 class HomePageView(LoginRequiredMixin, View):
     def get(self, request):
-        cv = show_cv(request.user)
-        return render(request, 'cv_hub/home_page.html', {'cv': cv})
+        if request.user.is_superuser:
+            cvs = get_all_cvs()  # superuser 
+            cv = None
+        else:
+            cv = show_cv(request.user) 
+            cvs = None
+
+        return render(request, 'cv_hub/home_page.html', {
+            'cv': cv,
+            'cvs': cvs
+        })
